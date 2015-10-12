a7c2e2dc1993a5fa9cdd587dc92ecf1535815756
Bardo91 <19Bardo91@gmail.com>
2015-10-12 12:54:03 +0200

    Checking if system has pcl and its dependencies. Add some guard in code to disable pcl methods

--- cpp/projects/calibrationTest/src/main.cpp
+++ cpp/projects/calibrationTest/src/main.cpp
@@ -10,7 +10,9 @@
 #include <fstream>
 #include "StereoCameras.h"
 
-#include <pcl-1.7/pcl/visualization/cloud_viewer.h>
+#ifdef ENABLE_PCL
+	#include <pcl-1.7/pcl/visualization/cloud_viewer.h>
+#endif
 
 using namespace std;
 using namespace cv;
@@ -35,7 +37,9 @@ int main(int _argc, char** _argv) {
 	//stereoCameras.save("stereo_A");
 	stereoCameras.load("stereo_A");
 
-	pcl::visualization::CloudViewer viewer("Simple Cloud Viewer");
+	#ifdef ENABLE_PCL
+		pcl::visualization::CloudViewer viewer("Simple Cloud Viewer");
+	#endif
 	Mat frame1, frame2;
 	for (;;) {
 		std::cout << "Getting frames" << std::endl;
@@ -51,22 +55,25 @@ int main(int _argc, char** _argv) {
 		if(points3d.size() == 0)
 			continue;
 
-		std::cout << "Filling Point cloud" << std::endl;
-		pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);  //fill the cloud.
+		#ifdef ENABLE_PCL
+			std::cout << "Filling Point cloud" << std::endl;
+			pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);  //fill the cloud.
 
-		//double temp_x , temp_y , temp_z;
-		for (unsigned i = 0; i < points3d.size(); i++) {
-			if (points3d[i].x > -3000 && points3d[i].x < 3000) {
-				if (points3d[i].y > -3000 && points3d[i].y < 3000) {
-					if (points3d[i].z > 0 && points3d[i].z < 3000) {
-						pcl::PointXYZ point(points3d[i].x, points3d[i].y, points3d[i].z);
-						cloud->push_back(point);
+			//double temp_x , temp_y , temp_z;
+			for (unsigned i = 0; i < points3d.size(); i++) {
+				if (points3d[i].x > -3000 && points3d[i].x < 3000) {
+					if (points3d[i].y > -3000 && points3d[i].y < 3000) {
+						if (points3d[i].z > 0 && points3d[i].z < 3000) {
+							pcl::PointXYZ point(points3d[i].x, points3d[i].y, points3d[i].z);
+							cloud->push_back(point);
+						}
 					}
 				}
 			}
-		}
 
-		viewer.showCloud(cloud);
+			viewer.showCloud(cloud);
+		#endif
+
 		Mat display;
 		hconcat(frame1, frame2, display);
 		imshow("display", display);
