319a83c8b62c1aa824993ab3f33a035d83f84468
Bardo91 <19Bardo91@gmail.com>
2015-10-20 09:43:05 +0200

    Changed sources and added depth color to points.

--- cpp/projects/calibrationTest/src/main.cpp
+++ cpp/projects/calibrationTest/src/main.cpp
@@ -12,7 +12,7 @@
 #include "TimeTools.h"
 
 #ifdef ENABLE_PCL
-	#include <pcl-1.7/pcl/visualization/cloud_viewer.h>
+	#include <pcl/visualization/cloud_viewer.h>
 #endif
 
 using namespace std;
@@ -22,8 +22,8 @@ int main(int _argc, char** _argv) {
 	/*vector<Mat> calibrationFrames1, calibrationFrames2;
 	for (unsigned i = 0; true; i++) {
 		// Load image
-		Mat frame1 = imread("/home/bardo91/programming/Datasets/Calibration_D/Calibration_D/SmallBoard/img_cam1_" + to_string(i) + ".jpg");
-		Mat frame2 = imread("/home/bardo91/programming/Datasets/Calibration_D/Calibration_D/SmallBoard/img_cam2_" + to_string(i) + ".jpg");
+		Mat frame1 = imread("C:/programming/Calibration D/Calibration D/SmallBoard/img_cam1_" + to_string(i) + ".jpg");
+		Mat frame2 = imread("C:/programming/Calibration D/Calibration D/SmallBoard/img_cam2_" + to_string(i) + ".jpg");
 		if (frame1.rows == 0 || frame2.rows == 0)
 			break;
 
@@ -36,7 +36,7 @@ int main(int _argc, char** _argv) {
 
 	stereoCameras.calibrate(calibrationFrames1, calibrationFrames2, Size(15, 10), 22.3);
 	stereoCameras.save("stereo_D");*/
-	StereoCameras stereoCameras("/home/bardo91/programming/Datasets/Calibration_D/Calibration_D/LargeRandom_highFPS/img_cam1_%d.jpg", "/home/bardo91/programming/Datasets/Calibration_D/Calibration_D/LargeRandom_highFPS/img_cam2_%d.jpg");
+	StereoCameras stereoCameras("C:/programming/Calibration D/Calibration D/LargeRandom_highFPS/img_cam1_%d.jpg", "C:/programming/Calibration D/Calibration D/LargeRandom_highFPS/img_cam2_%d.jpg");
 	stereoCameras.load("stereo_D");
 
 	#ifdef ENABLE_PCL
@@ -57,20 +57,26 @@ int main(int _argc, char** _argv) {
 		double t2 = timer->getTime();
 		vector<Point3f> points3d = stereoCameras.pointCloud(frame1, frame2);
 		double t3 = timer->getTime();
-		std::cout << "Time undistort: \t" << t1 - t0 << "\t FPS: " << 1/(t1-t0) << std::endl;
-		std::cout << "Time get point cloud:\t " << t3 - t2 << "\t FPS: " << 1/(t3-t2)<< std::endl;
+		std::cout << "Time undistort: /t" << t1 - t0 << "/t FPS: " << 1/(t1-t0) << std::endl;
+		std::cout << "Time get point cloud:/t " << t3 - t2 << "/t FPS: " << 1/(t3-t2)<< std::endl;
 		if(points3d.size() == 0)
 			continue;
 
 		#ifdef ENABLE_PCL
-			pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);  //fill the cloud.
+			pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>);  //fill the cloud.
 
 			//double temp_x , temp_y , temp_z;
 			for (unsigned i = 0; i < points3d.size(); i++) {
 				if (points3d[i].x > -3000 && points3d[i].x < 3000) {
 					if (points3d[i].y > -3000 && points3d[i].y < 3000) {
 						if (points3d[i].z > 0 && points3d[i].z < 1500) {
-							pcl::PointXYZ point(points3d[i].x, points3d[i].y, points3d[i].z);
+							pcl::PointXYZRGB point;
+							point.x = points3d[i].x;
+							point.y = points3d[i].y;
+							point.z = points3d[i].z;
+							point.r = 255*abs(points3d[i].x)/3000;
+							point.g = 255 - 255*abs(points3d[i].y)/3000;
+							point.b = 255*abs(points3d[i].z)/1500;
 							cloud->push_back(point);
 						}
 					}
