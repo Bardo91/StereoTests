226ec5c9cc0f148a24be25a6da27f31625c34da4
Bardo91 <19Bardo91@gmail.com>
2015-10-22 13:18:24 +0200

    Detecting if images are blurry or not discard them before processing PointCloud

--- cpp/projects/calibrationTest/src/ImageFilteringTools.cpp
+++ cpp/projects/calibrationTest/src/ImageFilteringTools.cpp
@@ -12,6 +12,7 @@ using namespace std;
 
 //---------------------------------------------------------------------------------------------------------------------
 bool isBlurry(const Mat &_image, double _threshold) {
+	// Calculate horizontal blur
 	Mat dx, ddx;
 	// Calculate first derivative (edge detector)
 	Sobel(_image, dx, CV_16S, 1, 0, 3, 1, 0, cv::BORDER_DEFAULT);
@@ -22,6 +23,21 @@ bool isBlurry(const Mat &_image, double _threshold) {
 	Laplacian(dx, ddx, CV_16S, 1, 1, 0, cv::BORDER_DEFAULT);
 	convertScaleAbs( ddx, ddx );
 
-	return double(sum(dx)[0]) / sum(ddx)[0] < _threshold;
+	double horiBlurMetric = double(sum(dx)[0]) / sum(ddx)[0];
+
+	// Calculate vertical blur
+	Mat dy, ddy;
+	// Calculate first derivative (edge detector)
+	Sobel(_image, dy, CV_16S, 1, 1, 3, 1, 0, cv::BORDER_DEFAULT);
+	convertScaleAbs( dy, dy );
+	threshold( dy, dy, 50, 255, THRESH_BINARY);
+
+	// Calculate second derivative (zero cross detector)
+	Laplacian(dy, ddy, CV_16S, 1, 1, 0, cv::BORDER_DEFAULT);
+	convertScaleAbs(ddy, ddy);
+
+	double vertBlurMetric = double(sum(dy)[0]) / sum(ddy)[0];
+
+	return horiBlurMetric > _threshold || vertBlurMetric > _threshold? true : false;
 }
 
--- cpp/projects/calibrationTest/src/main.cpp
+++ cpp/projects/calibrationTest/src/main.cpp
@@ -8,9 +8,11 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/xfeatures2d.hpp>
 #include <fstream>
+
 #include "StereoCameras.h"
 #include "TimeTools.h"
 #include "EnvironmentMap.h"
+#include "ImageFilteringTools.h"
 
 #ifdef ENABLE_PCL
 #include <pcl/visualization/cloud_viewer.h>
@@ -56,37 +58,49 @@ int main(int _argc, char** _argv) {
 
 		cvtColor(frame1, frame1, CV_BGR2GRAY);
 		cvtColor(frame2, frame2, CV_BGR2GRAY);
+		
+		double cBlurThreshold = 0.75;
+		bool isBlurry1 = isBlurry(frame1, cBlurThreshold);
+		bool isBlurry2 = isBlurry(frame2, cBlurThreshold);
+		if (isBlurry1 || isBlurry2) {
+			cvtColor(frame1, frame1, CV_GRAY2BGR);
+			cvtColor(frame2, frame2, CV_GRAY2BGR);
+			if(isBlurry1)
+				putText(frame1, "Blurry Image", Point2i(20,30), FONT_HERSHEY_SIMPLEX, 1.0, Scalar(0,0,255), 5);
+			if(isBlurry2)
+				putText(frame2, "Blurry Image", Point2i(20,30), FONT_HERSHEY_SIMPLEX, 1.0, Scalar(0,0,255), 5);
+		} else {
+			double t2 = timer->getTime();
+			vector<Point3f> points3d = stereoCameras.pointCloud(frame1, frame2);
+			double t3 = timer->getTime();
+			cout << "Time undistort: \t" << t1 - t0 << "\t FPS: " << 1 / (t1 - t0) << endl;
+			cout << "Time get point cloud:\t " << t3 - t2 << "\t FPS: " << 1 / (t3 - t2) << endl;
+			if (points3d.size() == 0)
+				continue;
 
-		double t2 = timer->getTime();
-		vector<Point3f> points3d = stereoCameras.pointCloud(frame1, frame2);
-		double t3 = timer->getTime();
-		cout << "Time undistort: \t" << t1 - t0 << "\t FPS: " << 1 / (t1 - t0) << endl;
-		cout << "Time get point cloud:\t " << t3 - t2 << "\t FPS: " << 1 / (t3 - t2) << endl;
-		if (points3d.size() == 0)
-			continue;
-
-		pcl::PointCloud<PointXYZ> cloud;
-		#ifdef ENABLE_PCL
-		//double temp_x , temp_y , temp_z;
-		for (unsigned i = 0; i < points3d.size(); i++) {
-			if (points3d[i].x > -3000 && points3d[i].x < 3000) {
-				if (points3d[i].y > -3000 && points3d[i].y < 3000) {
-					if (points3d[i].z > 650 && points3d[i].z < 1500) {
-						PointXYZ point(points3d[i].x, points3d[i].y, points3d[i].z);
-						cloud.push_back(point);
+			pcl::PointCloud<PointXYZ> cloud;
+			#ifdef ENABLE_PCL
+			//double temp_x , temp_y , temp_z;
+			for (unsigned i = 0; i < points3d.size(); i++) {
+				if (points3d[i].x > -3000 && points3d[i].x < 3000) {
+					if (points3d[i].y > -3000 && points3d[i].y < 3000) {
+						if (points3d[i].z > 650 && points3d[i].z < 1500) {
+							PointXYZ point(points3d[i].x, points3d[i].y, points3d[i].z);
+							cloud.push_back(point);
+						}
 					}
 				}
 			}
-		}
 
-		cloud = map3d.filter(cloud);
-		map3d.addPoints(cloud);
-		viewer.showCloud(map3d.cloud().makeShared());
-		#endif
+			cloud = map3d.filter(cloud);
+			map3d.addPoints(cloud);
+			viewer.showCloud(map3d.cloud().makeShared());
+			#endif
+		}
 
 		Mat display;
 		hconcat(frame1, frame2, display);
 		imshow("display", display);
-		waitKey(1);
+		waitKey();
 	}
 }
