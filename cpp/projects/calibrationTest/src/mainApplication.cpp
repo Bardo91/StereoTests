157a5d50a634b62beb0eb21e1a2fde73f929b7ed
Bardo91 <19Bardo91@gmail.com>
2015-10-29 20:59:43 +0100

    Added configuration file in a json. WIP: refactoring, moving main to mainApplication class

--- cpp/projects/calibrationTest/src/mainApplication.cpp
+++ cpp/projects/calibrationTest/src/mainApplication.cpp
@@ -6,29 +6,45 @@
 
 
 #include "Gui.h"
+#include "ImageFilteringTools.h"
 #include "mainApplication.h"
 
-#include <iostream>
+#include <cassert>
 #include <fstream>
+#include <iostream>
 
 using namespace cjson;
 using namespace cv;
+using namespace pcl;
 using namespace std;
 
 //---------------------------------------------------------------------------------------------------------------------
-bool MainApplication::init(int _argc, char ** _argv) {
+MainApplication::MainApplication(int _argc, char ** _argv) {
 	bool result = true;
 	result &= loadArguments(_argc, _argv);
 	result &= initGui();
 	result &= initCameras();
 	result &= init3dMap();
 
-	return result;
+	assert(result);
 }
 
 //---------------------------------------------------------------------------------------------------------------------
 bool MainApplication::step() {
-	return false;
+	Mat frame1, frame2;
+	if(!stepGetImages(frame1, frame2)) return false;
+
+	std::vector<cv::Point3f> points3d;
+	if(!stepTriangulatePoints(frame1, frame2, points3d)) return false;
+
+	pcl::PointCloud<pcl::PointXYZ>::Ptr newCloud;
+	if(!stepUpdateMap(points3d, newCloud)) return false;
+
+	if(!stepUpdateCameraRotation()) return false;
+
+	if(!stepGetCandidates(newCloud)) return false;
+
+	return true;
 }
 
 //---------------------------------------------------------------------------------------------------------------------
@@ -46,15 +62,17 @@ bool MainApplication::loadArguments(int _argc, char ** _argv) {
 //---------------------------------------------------------------------------------------------------------------------
 bool MainApplication::initGui() {
 	Gui::init(mConfig["gui"]["name"]);
-
-	return true;
+	mGui = Gui::get();
+	return mGui != nullptr ? true: false;
 }
 
 //---------------------------------------------------------------------------------------------------------------------
 bool MainApplication::initCameras(){
 	StereoCameras stereoCameras(mConfig["cameras"]["left"], mConfig["cameras"]["right"]);
+	Json leftRoi = mConfig["cameras"]["leftRoi"];
+	Json rightRoi = mConfig["cameras"]["rightRoi"];
+	stereoCameras.roi(Rect(leftRoi[0],leftRoi[1],leftRoi[2],leftRoi[3]), Rect(rightRoi[0],rightRoi[1],rightRoi[2],rightRoi[3]));
 	stereoCameras.load(mConfig["cameras"]["paramFile"]);
-
 	return true;
 }
 
@@ -69,16 +87,12 @@ bool MainApplication::init3dMap(){
 	params.icpEuclideanEpsilon					= mConfig["mapParams"]["icpEuclideanEpsilon"];			//1e-20; //seems to have no influence, implies local minimum found
 	params.icpMaxIcpIterations					= mConfig["mapParams"]["icpMaxIcpIterations"];			//1000; //no increase in time.. meaning we reach exit condition much sooner
 	params.icpMaxCorrespondenceDistance			= mConfig["mapParams"]["icpMaxCorrespondenceDistance"]; //0.1; //had it at 1 meter, now reduced it to 10 cm... results similar
-	params.icpMaxCorrDistDownStep				= mConfig["mapParams"]["icpMaxCorrDistDownStep"];		//0.01;
-	params.icpMaxCorrDistDownStepIterations		= mConfig["mapParams"]["icpMaxCorrDistDownStepIterations"]; //1;
 	params.historySize							= (int) mConfig["mapParams"]["historySize"];			//2;
 	params.clusterTolerance						= mConfig["mapParams"]["clusterTolerance"];				//0.035; //tolerance for searching neigbours in clustering. Points further apart will be in different clusters
 	params.minClusterSize						= mConfig["mapParams"]["minClusterSize"];				//15;
 	params.maxClusterSize						= mConfig["mapParams"]["maxClusterSize"];				//200;
 	params.floorDistanceThreshold				= mConfig["mapParams"]["floorDistanceThreshold"];		//0.01;
-	params.floorMaxIters						= (int) mConfig["mapParams"]["floorMaxIters"];			//1000;
-	params.floorCameraMinAngle					= mConfig["mapParams"]["floorCameraMinAngle"];			//M_PI/180 * 180;
-	params.floorCameraMaxAngle					= mConfig["mapParams"]["floorCameraMaxAngle"];			//M_PI/180 * 30;
+	params.floorMaxIters						= (int) mConfig["mapParams"]["floorMaxIters"];			//M_PI/180 * 30;
 
 	mMap.params(params);
 
@@ -86,104 +100,106 @@ bool MainApplication::init3dMap(){
 }
 
 //---------------------------------------------------------------------------------------------------------------------
-bool MainApplication::stepSearchPointsOnImage(vector<Point3f> &_points3d){
-	Mat frame1, frame2;
-	mCameras.frames(frame1, frame2, StereoCameras::eFrameFixing::Undistort);
-	gui->updateStereoImages(frame1, frame2);
-
-	cvtColor(frame1, frame1, CV_BGR2GRAY);
-	cvtColor(frame2, frame2, CV_BGR2GRAY);
-
-	double cBlurThreshold = 0.8;
-	bool isBlurry1 = isBlurry(frame1, cBlurThreshold);
-	bool isBlurry2 = isBlurry(frame2, cBlurThreshold);
-	if(isBlurry1) 
-		gui->putBlurry(true);
-	if(isBlurry2) 
-		gui->putBlurry(false);
-
-	if (isBlurry1 || !isBlurry2) 
+bool MainApplication::stepGetImages(cv::Mat & _frame1, cv::Mat & _frame2) {
+	mCameras.frames(_frame1, _frame2, StereoCameras::eFrameFixing::Undistort);
+	if (_frame1.rows == 0)
 		return false;
 
-	points3d = stereoCameras.pointCloud(frame1, frame2);	
-	if (points3d.size() == 0)
+	mGui->updateStereoImages(_frame1, _frame2);
+	Rect leftRoi = mCameras.roi(true);
+	Rect rightRoi = mCameras.roi(false);
+	mGui->drawBox(leftRoi, true, 0,255,0);
+	mGui->drawBox(rightRoi, false, 0,255,0);
+
+	cvtColor(_frame1, _frame1, CV_BGR2GRAY);
+	cvtColor(_frame2, _frame2, CV_BGR2GRAY);
+
+	bool isBlurry1 = isBlurry(_frame1, mConfig["blurThreshold"]);
+	bool isBlurry2 = isBlurry(_frame2, mConfig["blurThreshold"]);
+	if(isBlurry1) 
+		mGui->putBlurry(true);
+	if(isBlurry2) 
+		mGui->putBlurry(false);
+	
+	if(isBlurry1 || isBlurry2)
 		return false;
 
 	return true;
 }
 
 //---------------------------------------------------------------------------------------------------------------------
-bool MainApplication::stepUpdateMap(vector<Point3f> &_points3d, PointCloud<PointXYZ>::Ptr &_cloud){
-	PointCloud<PointXYZ> cloud;
-	//double temp_x , temp_y , temp_z;
-	for (unsigned i = 0; i < points3d.size(); i++) {
-		if (points3d[i].x > -3 && points3d[i].x < 3) {
-			if (points3d[i].y > -3 && points3d[i].y < 3) {
-				if (points3d[i].z > 0.65 && points3d[i].z < 1.5) {
-					PointXYZ point(points3d[i].x, points3d[i].y, points3d[i].z);
-					cloud.push_back(point);
+bool MainApplication::stepTriangulatePoints(const cv::Mat &_frame1, const cv::Mat &_frame2, std::vector<cv::Point3f> &_points3d){
+	_points3d = mCameras.pointCloud(_frame1, _frame2);	
+
+	return _points3d.size() != 0? true:false;
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+bool MainApplication::stepUpdateMap(const vector<Point3f> &_points3d, PointCloud<PointXYZ>::Ptr &_cloud){
+	for (unsigned i = 0; i < _points3d.size(); i++) {
+		if (_points3d[i].x > -3 && _points3d[i].x < 3) {
+			if (_points3d[i].y > -3 && _points3d[i].y < 3) {
+				if (_points3d[i].z > 0.65 && _points3d[i].z < 1.5) {
+					PointXYZ point(_points3d[i].x, _points3d[i].y, _points3d[i].z);
+					_cloud->push_back(point);
 				}
 			}
 		}
 	}
 
-	map3d.addPoints(cloud.makeShared());
-	gui->clearMap();
-	gui->clearPcViewer();
-	gui->drawMap(map3d.cloud().makeShared());
-	gui->addPointToPcViewer(cloud.makeShared());
+	mMap.addPoints(_cloud->makeShared());
+	mGui->clearMap();
+	mGui->clearPcViewer();
+	mGui->drawMap(mMap.cloud().makeShared());
+	mGui->addPointToPcViewer(_cloud->makeShared());
+	return true;
 }
 
 //---------------------------------------------------------------------------------------------------------------------
-bool MainApplication::stepGetCandidates(){
-	vector<PointIndices> mClusterIndices;
-	PointCloud<PointXYZ>::Ptr currentViewCleanedCloud;
-	currentViewCleanedCloud = map3d.voxel(map3d.filter(cloud.makeShared()));
-	PointCloud<PointXYZ>::Ptr cloudForProcessing;
-	bool useMapForClusters = true;
-	if(useMapForClusters)
-		cloudForProcessing = map3d.cloud().makeShared();
-	else
-		cloudForProcessing = currentViewCleanedCloud;
+bool MainApplication::stepUpdateCameraRotation() {
+	//sorry but I didn't find a better way to transform between cv and eigen, there is a function eigen2cv but I have problems
+	Mat R(3,3, CV_64F), T(3,1, CV_64F);
+	Eigen::Matrix4f a = mMap.lastView2MapTransformation().inverse();
+	cout  << "eigen: " << endl << a << endl;
+	R.at<double>(0, 0) = a(0, 0);
+	R.at<double>(0, 1) = a(0, 1);
+	R.at<double>(0, 2) = a(0, 2);
+	R.at<double>(1, 0) = a(1, 0);
+	R.at<double>(1, 1) = a(1, 1);
+	R.at<double>(1, 2) = a(1, 2);
+	R.at<double>(2, 0) = a(2, 0);
+	R.at<double>(2, 1) = a(2, 1);
+	R.at<double>(2, 2) = a(2, 2);
+	cout << "R: " << endl << R << endl;
+	T.at<double>(0, 0) = a(0, 3);
+	T.at<double>(1, 0) = a(1, 3);
+	T.at<double>(2, 0) = a(2, 3);
+	cout << "T: " << endl << T << endl;
 
-	ModelCoefficients plane = map3d.extractFloor(map3d.cloud().makeShared());
-	gui->drawPlane(plane, 0,0,1.5);
-	PointCloud<PointXYZ>::Ptr cropedCloud = cloudForProcessing;
-	map3d.cropCloud(cropedCloud, plane);
-	vector<PointCloud<PointXYZ>::Ptr> clusters;
-	map3d.clusterCloud(cloudForProcessing, clusters);
-
-
-	for (PointCloud<PointXYZ>::Ptr cluster : clusters) {
-		gui->addCluster(cluster, 3, rand()*255/RAND_MAX, rand()*255/RAND_MAX, rand()*255/RAND_MAX);
-		vector<Point3f> clusterPoints;
-		if (useMapForClusters) {
-			Eigen::Matrix4f invT = map3d.lastView2MapTransformation().inverse();
-			PointCloud<PointXYZ> clusterFromCameraView;
-			transformPointCloud(*cluster, clusterFromCameraView, invT);
-			for (const PointXYZ point : clusterFromCameraView)
-				clusterPoints.push_back(Point3f(point.x, point.y, point.z));
-		}
-		else {
-			for (const PointXYZ point : *cluster)
-				clusterPoints.push_back(Point3f(point.x, point.y, point.z));
-		}
-
-		vector<Point2f> reprojection1, reprojection2;
-		projectPoints(clusterPoints, Mat::eye(3, 3, CV_64F), Mat::zeros(3, 1, CV_64F),stereoCameras.camera(0).matrix(), stereoCameras.camera(0).distCoeffs(), reprojection1);
-		projectPoints(clusterPoints, stereoCameras.rotation(), stereoCameras.translation(), stereoCameras.camera(1).matrix(), stereoCameras.camera(1).distCoeffs(), reprojection2);
-		unsigned r, g, b; r = rand() * 255 / RAND_MAX; g = rand() * 255 / RAND_MAX; b = rand() * 255 / RAND_MAX;
-		gui->addCluster(cluster, 3, r, g, b);
-		gui->drawPoints(reprojection1, true, r, g, b);
-		gui->drawPoints(reprojection2, false, r, g, b);
-		// Calculate convexHull
-		vector<Point2f> convexHull1, convexHull2;
-		convexHull(reprojection1, convexHull1);
-		convexHull(reprojection2, convexHull2);
-		gui->drawPolygon(convexHull1, true, r, g, b);
-		gui->drawPolygon(convexHull2, false, r, g, b);
-
-		cout << "PointCloud representing the Cluster: " << cluster->points.size() << " data points." << endl;
-	}
+	mCameras.updateGlobalRT(R, T);	
 }
+
+//---------------------------------------------------------------------------------------------------------------------
+bool MainApplication::stepGetCandidates(const pcl::PointCloud<pcl::PointXYZ>::Ptr &_newCloud){
+	std::vector<pcl::PointIndices> mClusterIndices;
+	pcl::PointCloud<PointXYZ>::Ptr currentViewCleanedCloud;
+	currentViewCleanedCloud = mMap.voxel(mMap.filter(_newCloud.makeShared()));
+	pcl::PointCloud<PointXYZ>::Ptr cloudForProcessing;
+	cloudForProcessing = mMap.cloud().makeShared();
+
+	ModelCoefficients plane = mMap.extractFloor(mMap.cloud().makeShared());
+	mGui->drawPlane(plane, 0,0,1.5);
+	PointCloud<PointXYZ>::Ptr cropedCloud = cloudForProcessing;
+	mMap.cropCloud(cropedCloud, plane);
+	mClusterIndices = mMap.clusterCloud(cropedCloud);
+
+	std::vector<ObjectCandidate> candidates;
+	//create candidates from indices
+	for (pcl::PointIndices indices : mClusterIndices)
+		candidates.push_back(ObjectCandidate(indices, cloudForProcessing, true));
+	//draw all candidates
+	for (ObjectCandidate candidate : candidates) {
+		if(mMap.distanceToPlane(candidate.cloud(), plane) < 0.05)	// Draw only candidates close to the floor.
+			mGui->drawCandidate(candidate);	
+	}
 }
--- cpp/projects/calibrationTest/src/mainApplication.h
+++ cpp/projects/calibrationTest/src/mainApplication.h
@@ -8,15 +8,18 @@
 #ifndef MAINAPPLICATION_H_
 #define MAINAPPLICATION_H_
 
-#include "StereoCameras.h"
+#include "Gui.h"
 #include "EnvironmentMap.h"
+#include "StereoCameras.h"
 
 #include <cjson/json.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
 #include <opencv2/opencv.hpp>
 
 class MainApplication {
 public:
-	bool init	(int _argc, char** _argv);
+	MainApplication	(int _argc, char** _argv);
 	bool step	();
 private:
 	bool loadArguments	(int _argc, char** _argv);
@@ -24,13 +27,16 @@ private:
 	bool initCameras	();
 	bool init3dMap		();
 
-	bool stepSearchPointsOnImage(std::vector<cv::Point3f> &_points3d);
-	bool stepUpdateMap(std::vector<cv::Point3f> &_points3d, pcl::PointCloud<pcl::PointXYZ>::Ptr &_cloud);
-	bool stepGetCandidates();
+	bool stepGetImages(cv::Mat &_frame1, cv::Mat &_frame2);
+	bool stepTriangulatePoints(const cv::Mat &_frame1, const cv::Mat &_frame2, std::vector<cv::Point3f> &_points3d);
+	bool stepUpdateMap(const std::vector<cv::Point3f> &_points3d, pcl::PointCloud<pcl::PointXYZ>::Ptr &_cloud);
+	bool stepUpdateCameraRotation();
+	bool stepGetCandidates(const pcl::PointCloud<pcl::PointXYZ>::Ptr &_newCloud);
 
 private:
 	StereoCameras	mCameras;
 	EnvironmentMap	mMap;
+	Gui				*mGui;
 
 	cjson::Json mConfig;
 };
