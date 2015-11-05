8560d8a63f4b95ae59b1700e35da3055b82c4b62
Bardo91 <19Bardo91@gmail.com>
2015-11-05 14:47:12 +0100

    Created a class that stores the recognition system. And added extra fields to json file.

---
+++ cpp/projects/calibrationTest/src/vision/RecognitionSystem.cpp
@@ -0,0 +1,119 @@
+//
+//
+//
+//
+//
+//
+
+
+#include "RecognitionSystem.h"
+
+
+using namespace algorithm;
+using namespace cjson;
+using namespace cv;
+using namespace std;
+
+//---------------------------------------------------------------------------------------------------------------------
+// Public interface
+//---------------------------------------------------------------------------------------------------------------------
+RecognitionSystem::RecognitionSystem(cjson::Json _configFile) {
+	setBowParams(_configFile["bowParams"]);
+	setModel(_configFile["mlModel"]);
+	setFeatures(_configFile["features"]);
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+void RecognitionSystem::cathegorize(ObjectCandidate & _candidate) {
+
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+// Private methods
+//---------------------------------------------------------------------------------------------------------------------
+void RecognitionSystem::setBowParams(cjson::Json _params) {
+	mBowParams.vocSize = (int) _params["vocabularySize"];
+	mBow.params(mBowParams);
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+cv::ml::SVM::Types RecognitionSystem::decodeSvmType(std::string _string){
+	if (_string == "c_svc") {
+		return ml::SVM::Types::C_SVC;
+	}
+	else if (_string == "v_svc") {
+		return ml::SVM::Types::NU_SVC;
+	}
+	else if (_string == "v_svr") {
+		return ml::SVM::Types::NU_SVR;
+	}
+	else if (_string == "eps_svr") {
+		return ml::SVM::Types::EPS_SVR;
+	}
+	else {
+		assert(false);
+		return ;
+	}
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+cv::ml::SVM::KernelTypes RecognitionSystem::decodeKernelType(std::string _string) {
+	if (_string == "c_svc") {
+		return ml::SVM::KernelTypes::RBF;
+	}
+	else {
+		assert(false);
+		return ;
+	}
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+void RecognitionSystem::setModel(cjson::Json _params) {
+	if (_params["name"] == "SVM") {
+		SvmModel model;
+		model.setParams(_params["c"], _params["gamma"], decodeSvmType(_params["svmType"]), decodeKernelType(_params["kernel"]));
+	}
+	else if (_params["name"] = "LDA") {
+
+	}
+	else {
+		assert();
+	}
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+void RecognitionSystem::setFeatures(cjson::Json _params) {
+	// get detector
+	if (_params["detector"] == "SIFT") {
+		mBowParams.extractorType = BoW::Params::eExtractorType::SIFT;
+	}
+	else if (_params["detector"] == "FAST") {
+		mBowParams.extractorType = BoW::Params::eExtractorType::FAST;
+	}
+	else if (_params["detector"] == "ORB") {
+		mBowParams.extractorType = BoW::Params::eExtractorType::ORB;
+	}
+	else if (_params["detector"] == "SURF") {
+		mBowParams.extractorType = BoW::Params::eExtractorType::SURF;
+	} else if (_params["detector"] == "MSER") {
+		mBowParams.extractorType = BoW::Params::eExtractorType::MSER;
+	}
+	else {
+		assert(false);
+	}
+
+
+	// Get descriptor
+	if (_params["detector"] == "SIFT") {
+		mBowParams.descriptorType = BoW::Params::eDescriptorType::SIFT;
+	}
+	else if (_params["detector"] == "SIFT") {
+		mBowParams.descriptorType = BoW::Params::eDescriptorType::ORB;
+	}
+	else if (_params["detector"] == "SIFT") {
+		mBowParams.descriptorType = BoW::Params::eDescriptorType::SURF;
+	}
+	else {
+		assert(false);
+	}
+}
---
+++ cpp/projects/calibrationTest/src/vision/RecognitionSystem.h
@@ -0,0 +1,43 @@
+//
+//
+//
+//
+//
+//
+
+
+
+
+
+#ifndef RECOGNITIONSYStEM_H_
+#define RECOGNITIONSYStEM_H_
+
+#include "ml/BoW.h"
+
+#include <vector>
+#include <cjson/Json.h>
+
+// Forward declaration
+class ObjectCandidate;
+
+class RecognitionSystem {
+public:		// Public interface.
+	RecognitionSystem(cjson::Json _configFile);
+
+	void cathegorize(ObjectCandidate &_candidate);
+
+private:	// Private methods.
+	void setBowParams(cjson::Json _params);
+	void setModel(cjson::Json _params);
+	cv::ml::SVM::Types decodeSvmType(std::string _string);
+	cv::ml::SVM::KernelTypes decodeKernelType(std::string _string);
+
+	void setFeatures(cjson::Json _params);
+
+private:	// Private members.
+	algorithm::BoW	mBow;
+	BoW::Params		mBowParams;
+};
+
+
+#endif	//	RECOGNITIONSYStEM_H_
