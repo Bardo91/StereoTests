0a3a14a0b4da5644008a522af2a19479224aee84
Bardo91 <19Bardo91@gmail.com>
2015-11-09 16:12:42 +0100

    Improved train application to test results of training

--- cpp/projects/trainingApp/src/main.cpp
+++ cpp/projects/trainingApp/src/main.cpp
@@ -1,6 +1,6 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //
-//		Author:	Pablo Ramon Soria
+//		Author:	Pablo Ramon SoriacvImagesPath
 //		Date:	2015-10-02
 //
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -16,19 +16,44 @@ int main(int _argc, char ** _argv) {
 	BoW::Params params;
 	params.extractorType = BoW::Params::eExtractorType::SIFT;
 	params.descriptorType = BoW::Params::eDescriptorType::SIFT;
-	params.imageSizeTrain = 640;
-	params.nScalesTrain = 3;
+	params.imageSizeTrain = 200;
+	params.nScalesTrain = 1;
 	params.scaleFactor = 0.5;
 	params.vocSize = 500;
 
 	bow.params(params);
 
 	SvmModel svm;
-	svm.setParams(0.1,2,cv::ml::SVM::Types::C_SVC, cv::ml::SVM::KernelTypes::RBF, true);
+	svm.setParams(2.25, 0.16384, cv::ml::SVM::Types::C_SVC,  cv::ml::SVM::KernelTypes::RBF, true);
 	bow.model(svm);
 
-	string imageTemplate = "C:/programming/datasets/objectsMaps/training (%d).jpg";
-	string gtFile = "C:/programming/datasets/objectsMaps/gt.txt";
+	string imageTemplate = "C:/programming/demo_bagofwords/dataset/datasetTools/train/img (%d).jpg";
+	string gtFile = "C:/programming/demo_bagofwords/dataset/datasetTools/train/gt.txt";
 	bow.train(imageTemplate, gtFile);
-	bow.save("canjuiboxcrasen.svm");
+	bow.save("tools");
+
+	string cvImagesPath = "C:/programming/demo_bagofwords/dataset/datasetTools/cv/";
+	vector<cv::Mat> cvImages;
+	for (int i = 1;i<99999;i++) {
+		cv::Mat image = cv::imread(cvImagesPath + "img (" + to_string(i) +").jpg");
+		if(image.rows == 0)
+			break;
+
+		cvImages.push_back(image);
+	}
+
+
+	vector<vector<pair<unsigned,float>>> results;
+	int index = 0;
+	for (cv::Mat image : cvImages) {
+		vector<cv::Rect> regions;
+		regions.push_back(cv::Rect(0, 0, image.cols, image.rows));
+		results.push_back(bow.evaluate(image, regions));
+
+		std::cout << "Image " << index << ". Label " << results[index][0].first << ". Prob " << results[results.size() -1][0].second << std::endl;
+		index++;
+	}
+
+	system("PAUSE");
+
 }
