34f7174c58d526f1a268dce9a8e6ebb2372cb3cd
Robert Bevec <robert.bevec@gmail.com>
2015-11-10 11:39:54 +0100

    Created zerocheck method, seems to work well

--- cpp/projects/objectsMap/src/ImageFilteringTools.h
+++ cpp/projects/objectsMap/src/ImageFilteringTools.h
@@ -15,6 +15,10 @@
 ///	\param _threshold: threshold of internal blur metric. Must be higher than 0.5 (Large value accept more blur on image).
 bool isBlurry(const cv::Mat &_image, double _threshold);
 
+void zeroCrossX(const cv::Mat &_binaryImage, cv::Mat &_output);
+
+void zeroCrossY(const cv::Mat &_binaryImage, cv::Mat &_output);
+
 
 
 #endif	//	IMAGEFILTERINGTOOLS_H_
