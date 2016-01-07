109337bc6404a990354b5bf76d26b2f35e0f840a
Bardo91 <19Bardo91@gmail.com>
2016-01-07 01:29:08 +0100

    Updated test data. And finished script to plot candidate probabilities.

--- Matlab/plotData/plotCandidateFile.m
+++ Matlab/plotData/plotCandidateFile.m
@@ -15,7 +15,14 @@ function plotCandidateFile(fileName)
     
     figure();
     hold on;
-    for i = 1:n
-        plot(probs(
+    legends = {};
+    
+    lineStyles = {'-','--','-o', '-*', '--o', '--*'};
+    
+    for i = 1:length(probs(:,1))
+        plot(probs(i,:),lineStyles{floor(i/7)+1});
+        legends{i} = strcat('label ', int2str(i-1));
     end
+    [h,icons,plots,str] = legend(legends);
+    set(icons(:),'LineWidth',2); %// Or whatever
 end
